7ba698a7a4a5a8905a9b7ad089c332f7ef46c855
elenatruniak <elena.truniak@gmail.com>
2022-06-21 18:42:28 +0200

    update test

--- test_git.py
+++ test_git.py
@@ -1,3 +1,15 @@
 
-def test_git():
-    assert 3 > 1
+def service_func():
+    print("Hi, I'm sevice func that you call everywhere")
+
+def test_more(input: int):
+    service_func()
+    assert input > 2
+
+def test_less(input: int):
+    service_func()
+    assert input < 2
+
+def main():
+    test_less(3)
+    test_more(3)
