70b0bc8f31263cc88f025f6caf35f6274acd43b0
elenatruniak <elena.truniak@gmail.com>
2022-06-21 18:57:55 +0200

    update test with comment1

--- test_git.py
+++ test_git.py
@@ -2,17 +2,14 @@
 
 def service_func():
     print("Hi, I'm sevice func that you call everywhere")
-
+#comment1
 def test_more(input: int):
     service_func()
     assert input > 2
 
-def test_less(input: int):
-    service_func()
-    assert input < 2
 
+#comment3
 def main():
-    test_less(1)
     test_more(3)
     print("ok")
 
